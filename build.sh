bde15e62158d28ae6cc933d87bb0eb5e92b705d8
Laurent Thévenoux <laurent@thevenoux.net>
2026-05-08 13:25:59 +0200

    Update to LLVM 21

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ mkdir -p build
 pushd build
 
 cmake -G Ninja .. \
-    -DMLIR_DIR=/usr/lib/llvm-20/lib/cmake/mlir \
-    -DCMAKE_C_COMPILER=clang-20 \
-    -DCMAKE_CXX_COMPILER=clang++-20 \
+    -DMLIR_DIR=/usr/lib/llvm-21/lib/cmake/mlir \
+    -DCMAKE_C_COMPILER=clang-21 \
+    -DCMAKE_CXX_COMPILER=clang++-21 \
     -DCMAKE_CXX_FLAGS="-gdwarf-4" \
     -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" \
     -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold" \
@@ -21,4 +21,4 @@ popd
 ./build/bin/lalvm --emit=ast test.adb
 ./build/bin/lalvm --emit=mlir test.adb -mlir-pretty-debuginfo
 ./build/bin/lalvm --emit=llvm test.adb
-./build/bin/lalvm --emit=llvm test.adb 2>&1 >/dev/null | llc-20 -o -
+./build/bin/lalvm --emit=llvm test.adb 2>&1 >/dev/null | llc-21 -o -
